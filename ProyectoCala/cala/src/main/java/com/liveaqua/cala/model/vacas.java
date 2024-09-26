1df4b3c16afd0b7edce174d99224a7ee766c7242
Rastir <aldojero@hotmail.com>
2024-09-26 15:59:45 -0500

    Cala deploy

---
+++ ProyectoCala/cala/src/main/java/com/liveaqua/cala/model/vacas.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.liveaqua.cala.model;
+
+/**
+ *
+ * @author aldoj
+ */
+public class vacas {
+    
+    String namr;
+    // esto es un comentario diciendo que estas son mis vacaciones dia 1 
+    // esto es un comentario diciendo que éstas son mis vacaciones, dia 2, fui de pesca con mi papá y mi esposa
+    // me la pasé increible, sacamos 5 lobinas y 2 truchas arcoiris, ha sido el mejor día de mi vida 17 de sep 2024,
+    // a un día de cumplir 30 :)
+    // esto es un comentario diciendo que estas son mis vacaciones, día 3, hoy estuve paseando con mi esposa y mi 
+    // padre por el centro de la ciudad, me ha llevado de comprar y tambien a realizarme un examen ortopedico, así
+    // podré tener una mejor calidad de vida para mi sin dolor en los pies.
+    // ESTO ES UN COMENTARIO DICIENDO QUE ESTAS SON MIS VACACIONES, DÍA 4, ESTOY EN SAN MIGUEL DE ALLENDE CON MI ESPOSA
+    // Y MIS ABUELOS, HEMOS VISITADO DOLORES HIDALGO Y COMIDO HELADO DE LAVANDA, HA ESTADO DELICIOSO, MAÑANA ESPERA
+    // UN DÍA DE SPA EN LAS AGUAS TERMALES "EL ESCONDIDO" DESPUES UNA VISITA AL CENTRO HISTORICO Y DESPUES UNA VUELTA
+    // AL SUPERMERCADO CON LA FAMILIA.
+    // Esto es un comentario diciendo que éstas son mis vacaciones, día 5, el día de hoy fui a un balneario con mi 
+    // familia, en especifico el "escondido" que despues de tantos años sigue siendo lo que recordaba de niño, solo que
+    // ahora de adulto lo puedo apreciar más, es hermoso, no habia tenido un momento de relajación así en un buen tiempo
+    // agradezco a mi Dios por mi familia y la oportunidad de disfrutar el momento que tengo con ellos mientras pueda
+    // solo mi señor sabe cuanto pueda disfrutar de esto.
+    // Fin de las vacaciones. 23 de Septiembre. 2024
+    //spring.datasource.url=jdbc:mysql://localhost:3306/aqualovers?useSSL=false&serverTimezone=UTC
+    
+}
