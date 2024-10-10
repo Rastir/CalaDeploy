2ee88198df32b220ed6d8c898b9faa9a715dafff
Rastir <aldojero@hotmail.com>
2024-10-10 16:15:16 -0500

    Re-run

--- clinica_veterinaria/src/main/java/com/todocodeacademy/clinica_veterinaria/CorsConfig.java
+++ clinica_veterinaria/src/main/java/com/todocodeacademy/clinica_veterinaria/CorsConfig.java
@@ -1,28 +1,21 @@
 package com.todocodeacademy.clinica_veterinaria;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://rastir.github.io/CalaWeb/modificarAqualovers.html","http://127.0.0.1:5500/modificarAqualovers.html")
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+
             }
-        };
-    }
-
-}
-
+        }
 // Se está trabajando en cambiar unas cosas para que se pueda consumir una api desde un html tanto en casero desde local host como desde un github pages, por hoy creo que encontramos el fallo
 
