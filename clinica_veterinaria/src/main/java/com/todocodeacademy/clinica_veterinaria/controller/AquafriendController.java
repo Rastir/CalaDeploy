a99144d59e8e42c5f87e07bc0d77c0f095c051fa
Rastir <aldojero@hotmail.com>
2025-02-10 12:40:08 -0500

    re-run

--- clinica_veterinaria/src/main/java/com/todocodeacademy/clinica_veterinaria/controller/AquafriendController.java
+++ clinica_veterinaria/src/main/java/com/todocodeacademy/clinica_veterinaria/controller/AquafriendController.java
@@ -42,7 +42,7 @@ public class AquafriendController {
         return "Aquafriend borrado correctamente. ¡Yey!";
     }
 
-    @PutMapping("/aquafriend/editar/")
+    @PutMapping("/aquafriend/editar")
     public String editAquafriend (@RequestBody Aquafriend aquaf){
         aquaServ.editAquafriend(aquaf);
 
